55a0468a4885c3a82ceb0fd5c54f1c9712b0a3f4
armerickson <104520822+armerickson@users.noreply.github.com>
2022-08-11 14:07:25 -0500

    fixed header icon

--- Desktop/travel-journal/src/components/Header.js
+++ Desktop/travel-journal/src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Header() {
     return (
         <header className="header">
-            <img className="header--world-icon" src="../favicon.ico" alt="Icon of a globe"/>
+            <img className="header--world-icon" src={require("../images/globe.png")} alt="Icon of a globe"/>
             <h4 className="header--title">my travel journal.</h4>
         </header>
     )
